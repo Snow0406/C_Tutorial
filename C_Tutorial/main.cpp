92d547338eace1350167f459ac892a662be71cce
Snow <87596507+Snow0406@users.noreply.github.com>
2024-03-30 00:23:29 +0900

    day01 - 2

--- C_Tutorial/main.cpp
+++ C_Tutorial/main.cpp
@@ -1,7 +1,29 @@
-#include <stdio.h> //include : 포함하다 (stdio.h를 들고 오는거예요)
+#include <stdio.h> //include : 포함하다 (stdio.h를 들고 오는거예요 - 그 파일에 있는 코드를 쓸꺼예요)
+
+//변수 : 수학시간떄 x, y
+//자료형 : 정수인지 문자열인지 주소인지 등등 문자인지 소수안지 등등 클래스 등등 void
+// // /* */ : 주석, 컴퓨터가 무시함
+int x = 5; //x : 변수 이름, int는 자료형이면서, 정수일때 사용을 해요.
+char y = 'c'; //y : 변수 이름, char는 자료형이면서, 문자일때 사용을 해요. 이거는 문자열은 사용 못함 (ex char y = '사로로인싸')
+float z = 3.14f; //z : 변수 이름, float은 자료형이면서, 소수일때 사용을 해요.
 
 int main() { //함수
 
-	printf("사로로 인싸 !");
+	//출력문
+	printf("%d", x);
+	//%d - int
+	
+	printf("%c", y);
+	//%c - char
+
+	printf("%c", z);
+	//%f - float
+
+	//printf("x : %d", x);
+
+	printf("x : %d, y : %c, z : %f", x, y, z);
+
+	//printf("x : %d, y : %c, z : %f", x, y, z);
+
 	return 0;
 }
