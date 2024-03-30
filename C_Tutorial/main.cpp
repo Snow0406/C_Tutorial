9cdcc0ae7cc6986550d1e561c7e5d3b7f19a72fd
Snow <87596507+Snow0406@users.noreply.github.com>
2024-03-31 00:33:33 +0900

    Day 02

--- C_Tutorial/main.cpp
+++ C_Tutorial/main.cpp
@@ -1,43 +1,103 @@
-#include <stdio.h> //include : 포함하다 (stdio.h를 들고 오는거예요 - 그 파일에 있는 코드를 쓸꺼예요)
+#include <stdio.h> 
 
-//변수 : 수학시간떄 x, y
-//자료형 : 정수인지 문자열인지 주소인지 등등 문자인지 소수안지 등등 클래스 등등 void
-// // /* */ : 주석, 컴퓨터가 무시함
+int main() {
 
-//x : 변수 이름
-//int는 자료형이면서, 정수일때 사용을 해요.
+	// \n : 줄바꿈
+	//printf("안녕하세요.\n저는 스노우 입니다");
 
-int x = 5; 
+	//float pi = 3.14f;
+	//printf("pi : %0.2f", pi);
 
-//y : 변수 이름
-//char는 자료형이면서, 문자일때 사용을 해요. 
-//이거는 문자열은 사용 못함 (ex char y = '사로로인싸')
+	//int x = 5;
 
-char y = 'c';
+	//int y;
+	//y = 5;
 
-//z : 변수 이름, 
-//float은 자료형이면서, 
-//소수일때 사용을 해요.
+	//x = 6;
+	//printf("%d", x);
 
-float z = 3.14f;
+	//int index;
+	//scanf_s("%d", &index);
 
-int main() { //함수
+	//printf("출력 : %d", index);
 
-	//출력문
-	//%d - int
-	printf("%d", x);
+	/* ========= 대입 연산자 (a = 2) ========= */
+
+	//수학은 a는 5다. 코딩에서는 a에다가 5를 넣는거예요.
+	//int a;
+	//a = 5;
+
+	/* ======== 산술 연산자[+-*(x), ÷(/ ), % (나머지)] ========== */
+	//int x = 10;
+	//int y = 3;
+
+	//int result;
+
+	//printf("%d + %d = %d", x, y, x + y);
+
+	//result = x + y;
+	//printf("%d + %d = %d", x, y, result);
+
+	//printf("%d - %d = %d", x, y, x - y);
+	//printf("%d x %d = %d", x, y, x * y);
+	//printf("%d ÷ %d = %d", x, y, x / y);
+
+	//%를 출력할때는 %% 2개 적어야 함, ex: % 2개 출력하고 싶으면 %%%%
+	//printf("%d %% %d = %d", x, y, x % y);
+
+	/* ========= 증감 연산자 (++, --) ========== */
+	//int num = 1;
 	
-	//%c - char
-	printf("%c", y);
+	//num에다가 num(1) + 1을 해줘 ---> num(2)
+	//num = num + 1;
+	//num++; //1 더하기
 
-	//%f - float
-	printf("%f", z);
+	//num에다가 num(1) - 1을 해줘 ---> num(0)
+    //num = num - 1;
+	//num--; //1 빼기
 
-	//printf("x : %d", x);
+	//printf("%d", num);
 
-	//printf("x : %d, y : %c, z : %f", x, y, z);
+	/* ========= 할당 연산자 (+=, -=, *= /=, %=) ========== */
+	//int num = 7;
 
-	//printf("x : %d, y : %c, z : %f", x, y, z);
+	//num = num + 2;
+	//num += 2;
+
+	//num -= 3; //(num = num - 3)
+
+	//num *= 3; //(num = num * 3)
+
+	//num /= 3; //(num = num / 3)
+
+	//num %= 3; //(num = num % 3)
+
+	//printf("%d", num);
+
+	/* ========= 관계 연산자 (==, !=, >, <) ========== */
+
+	//if - 만약 (ex : 만약 안녕이라고 하면 안녕하세요라고 말하기)
+
+	int num = 18;
+
+	//if (num == 18) { //if (조건문 - num이 18이라면) { // == : 같을때
+	//	printf("num은 18이야"); //출력
+	//}
+
+	if (num != 17) { //if (조건문 - num이 18이 아니라면) { // != : 다를때
+		printf("num은 18이 아니야"); //출력
+	}
+
+
+	//Good : if (num == 18) printf("num은 18이야"); 
+	//Bad : if (num == 18) { printf("num은 18이야"); printf("num은 18이야");printf("num은 18이야");printf("num은 18이야");printf("num은 18이야"); }
+
+	//if (true) {
+	//	printf("num은 18이야");
+	//	printf("num은 18이야");
+	//	printf("num은 18이야");
+	//	printf("num은 18이야");
+	//}
 
 	return 0;
 }
