bd678a93bc3c03c784353eb753a664fb28b2f78c
Snow <87596507+Snow0406@users.noreply.github.com>
2024-05-27 13:05:08 +0900

    add: day06 (struct)

--- C_Tutorial/day06.cpp
+++ C_Tutorial/day06.cpp
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <Windows> //Window 함수를 들고 옵니다
 
+int hp = 100;
+int mp = 100;
+int money = 10000;
+
+struct Player {
+ int hp = 100;
+ int mp;
+ int money;
+}
+
 int main() {
+
+using namespace std;
+
  Sleep(2000); //2초 휴식
  system("cls"); //콘솔 초기화
+
+Player snow; //<구조체 이름> <이름>
+
+snow.hp += 1000; //hp 100에다가 1000 더한거 == 1100
+snow.mp = 100; //mp에다가 100을 넣어
+snow.money = 10; //money에다가 10을 넣어
+
+cout << "hp : " << snow.hp << endl;
 }
+
+//1. 콘솔에 과목 목록 보여주고 과목 입력 (ex 수학)
+//2. 만약 수학에 이미 정보가 있다면 바꿀껀지 물어보고 (ex 네 아니요 입력
+//3. 만약 바꾼다면 성적 점수를 바꾸고 아니라면 저장되있는 점수를 보여주는
+
+//더 간지나게
+//3. 만약 바꾼다면 닉네임과 성적 점수를 넣고 아니라면 저장되있는 [닉네임]: [점수]의 목록을 다 나타내는거지
